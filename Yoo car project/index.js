85a875417a467a173f6ef69155c923256b5c17ae
Yogieeww <127382080+Yogieeww@users.noreply.github.com>
2023-06-11 21:49:51 +0530

    Add files via upload

---
+++ Yoo car project/index.js
@@ -0,0 +1,7 @@
+const run = () => {
+    console.log('im runnin..');
+}
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
